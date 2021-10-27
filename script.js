e7c149c00f31975e62268a28430492d4f392a375
Samuele Miotto <miotto.samuele@gmail.com>
2021-10-27 17:59:05 +0200

    js print base html

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+// selezione dei contenitori dove inserire l'html
+
+const coverContainer = document.querySelector('.cover');
+const thumbContainer = document.querySelector('.thumbnails');
+
+// inserisco l'html di base
+
+coverContainer.innerHTML = `<div class="cover-cont active"><img class="cover-img" src="${items[0]}" alt="${title[0]}"><div class="text-cont"><h2>${title[0]}</h2><p>${text[0]}</p></div></div>`
+
+thumbContainer.innerHTML += `<img class="thumb active" src="${items[0]}" alt="${title[0]}">`
+
+for (let i = 1; i < items.length; i++) {
+
+    thumbContainer.innerHTML += `<img class="thumb " src="${items[i]}" alt="${title[i]}">`
+    coverContainer.innerHTML += `<div class="cover-cont "><img class="cover-img" src="${items[i]}" alt="${title[i]}"><div class="text-cont"><h2>${title[i]}</h2><p>${text[i]}</p></div></div>`
+
+}
